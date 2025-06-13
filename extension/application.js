8e7c63c87700e6e14be93e843304c1ac66999c9f
Ben Ubois <ben@benubois.com>
2025-06-13 13:55:27 +0200

    Load stimulus.

---
+++ extension/application.js
@@ -0,0 +1,17 @@
+import { Application } from "./lib/stimulus.js"
+import TabInfoController from "../controllers/tab_info_controller.js"
+
+// Start the Stimulus application
+const application = Application.start()
+
+// Set debug mode for development
+application.debug = false
+console.log("Stimulus application started")
+
+// Register controllers
+application.register("tab-info", TabInfoController)
+
+// Make the application globally available
+window.Stimulus = application
+
+export default application

8e0b1c3f73504e123b90bdad295c2d11b31401ef
Ben Ubois <ben@benubois.com>
2025-06-16 15:44:30 +0200

    Testing updates

--- test/popup_test.rb
+++ test/popup_test.rb
@@ -1,8 +1,8 @@
-require_relative 'test_helper'
+require_relative "test_helper"
 
 class PopupTest < SystemTest
   def test_popup_loads
-    visit '/popup.html'
+    visit "/popup.html"
     puts page.title
     assert_equal "Feedbin Subscribe & Save", page.title
   end

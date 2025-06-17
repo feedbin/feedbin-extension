89f0f8c4706bc061b65239f6104ce4151309c478
Ben Ubois <ben@benubois.com>
2025-06-17 11:39:27 +0200

    Sign In -> Settings.

--- _test/popup_test.rb
+++ _test/popup_test.rb
@@ -2,13 +2,13 @@ require_relative "test_helper"
 
 class PopupTest < SystemTest
   def test_popup_loads
-    CapybaraMock.stub_request(:get, "https://example.com/sign-in")
+    CapybaraMock.stub_request(:get, "https://example.com/settings")
       .to_return(body: {amount: "Hello"}.to_json)
 
     visit "/index.html"
     click_button("Sign In")
     puts "--------------"
-    puts page.find(:css, "[data-sign-in-target=results]").text
+    puts page.find(:css, "[data-settings-target=results]").text
     puts "--------------"
     assert_equal "Feedbin Subscribe & Save", page.title
   end

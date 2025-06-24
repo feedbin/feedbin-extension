b2110f27c6400fad5da7cabab0cec266f372d339
Ben Ubois <ben@benubois.com>
2025-06-24 14:06:30 +0200

    Test updates

--- _test/test_helper.rb
+++ _test/test_helper.rb
@@ -11,28 +11,27 @@ ENV["JEKYLL_ENV"] = "test"
 
 PROJECT_ROOT = File.expand_path("..", __dir__)
 SCREENSHOTS_DIR = File.join(PROJECT_ROOT, "tmp", "screenshots")
+SITE = Jekyll::Site.new(Jekyll.configuration({
+  source: PROJECT_ROOT,
+  destination: File.join(PROJECT_ROOT, "tmp", "_site"),
+}))
+SITE.process
 
 FileUtils.mkdir_p(SCREENSHOTS_DIR)
 Dir.glob(File.join(SCREENSHOTS_DIR, "*.png")).each { |file| FileUtils.rm(file) }
 
-$site = Jekyll::Site.new(Jekyll.configuration({
-  source:  PROJECT_ROOT,
-  destination: File.join(PROJECT_ROOT, "tmp", "_site"),
-}))
-$site.process
-
-Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app)
+Capybara.register_driver(:cuprite) do
+  Capybara::Cuprite::Driver.new(it)
 end
 
-Capybara.configure do |config|
-  config.javascript_driver = :cuprite
-  config.default_driver = :cuprite
-  config.default_max_wait_time = 1
-  config.disable_animation = true
-  config.server = :puma
-  config.app = Rack::Builder.new do
-    run Rack::Directory.new($site.dest)
+Capybara.configure do
+  it.javascript_driver = :cuprite
+  it.default_driver = :cuprite
+  it.default_max_wait_time = 1
+  it.disable_animation = true
+  it.server = :puma
+  it.app = Rack::Builder.new do
+    run Rack::Directory.new(SITE.dest)
   end
 end
 
@@ -41,11 +40,21 @@ class SystemTest < Minitest::Test
   include Capybara::Minitest::Assertions
 
   def teardown
-    if failure
-      screenshot_path = File.join(SCREENSHOTS_DIR, "#{name}.png")
-      page.save_screenshot(screenshot_path)
-      failure.message << "\nScreenshot: #{screenshot_path}"
-    end
+    capture_screenshot_on_failure
+    reset_test_state
+  end
+
+  private
+
+  def capture_screenshot_on_failure
+    return unless failure
+
+    screenshot_path = File.join(SCREENSHOTS_DIR, "#{name}.png")
+    page.save_screenshot(screenshot_path)
+    failure.message << "\nScreenshot: #{screenshot_path}"
+  end
+
+  def reset_test_state
     Capybara.reset_sessions!
     Capybara.use_default_driver
     CapybaraMock.reset!
@@ -53,12 +62,12 @@ class SystemTest < Minitest::Test
 end
 
 def build_url(path)
-  URI.join($site.config["api_host"], $site.config["urls"][path]).to_s
+  URI.join(SITE.config["api_host"], SITE.config["urls"][path]).to_s
 end
 
 def sign_in
   CapybaraMock.stub_request(:post, build_url("authentication"))
-    .to_return(body: {page_token: "token"}.to_json)
+    .to_return(body: { page_token: "token" }.to_json)
 
   fill_in "Email", with: "example@example.com"
   fill_in "Password", with: "password"

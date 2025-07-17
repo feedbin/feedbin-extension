62c8bd993dd46ca12133d9ce2aa31758f38ad13a
Ben Ubois <ben@benubois.com>
2025-07-17 16:37:39 +0200

    Make sure destination exists.

--- _plugins/manifest_generator.rb
+++ _plugins/manifest_generator.rb
@@ -15,6 +15,7 @@ module Jekyll
       manifest = data[target]
       name = "manifest.json"
       path = File.join(site.dest, name)
+      FileUtils.mkdir_p(site.dest)
       File.write(path, JSON.pretty_generate(manifest))
       site.keep_files << name
     end

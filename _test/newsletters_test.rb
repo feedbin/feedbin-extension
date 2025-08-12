9f053c005322e4f86d4f30d5f12e8f9200992264
Ben Ubois <ben@benubois.com>
2025-08-12 16:53:34 +0200

    Misc tests

--- _test/newsletters_test.rb
+++ _test/newsletters_test.rb
@@ -56,23 +56,55 @@ class NewslettersTest < SystemTest
 
   def test_auto_submit
     body = {
-      token: "df.225",
-      verified_token: "ImRmLjIyNSI=--d2ccbec34cce7ee9215bf331a55962bda726b912",
-      numbers: "225",
-      email: "df.225@feedb.in",
+      token: "eovwr",
+      addresses: [],
+      tags: []
+    }
+
+    CapybaraMock.stub_request(:post, build_url("new_address"))
+      .to_return(body: body.to_json)
+
+    body = {
+      token: "custom.456",
+      verified_token: "ImN1c3RvbS40NTYi--abc123",
+      numbers: "456",
+      email: "custom.456@feedb.in",
       addresses: [
         {
           email: "example.864@feedb.in",
           description: "Example Description"
         },
-      ]
+      ],
     }
-    CapybaraMock.stub_request(:post, build_url("new_address"))
+    CapybaraMock.stub_request(:post, build_url("create_address"))
       .to_return(body: body.to_json)
 
     visit "/index.html"
     sign_in
     click_tab(:newsletters)
 
+    # Wait for initial data to load
+    assert_selector("[data-newsletters-target='addressInput']")
+
+    input = find("[data-newsletters-target='addressInput']")
+    assert_equal "eovwr", input.value
+
+    assert_selector("[data-newsletters-target='addressOutput']", text: "df.225@feedb.in")
+
+    input.set("custom")
+
+    assert_selector("[data-newsletters-target='addressOutput']", text: "custom.456@feedb.in")
+    assert_selector("[data-newsletters-target='numbers']", text: "456")
+
+    assert_equal "true", find("[data-newsletters-edited-value]").value
+
+    # Verify submit button was temporarily disabled and should re-enable after 500ms
+    submit_button = find("[data-newsletters-target='submitButton']")
+    # Button is disabled during auto-submit
+    assert submit_button.disabled?
+
+    # Wait for button to re-enable after timeout (500ms in the controller)
+    sleep 0.6
+    assert !submit_button.disabled?
   end
 end

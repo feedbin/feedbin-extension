61f9825ef6c1fdb28680dae21b4c4e628ece7b02
Ben Ubois <ben@benubois.com>
2025-08-12 14:41:18 +0200

    Tests

---
+++ _test/newsletters_test.rb
@@ -0,0 +1,56 @@
+require_relative "test_helper"
+
+class NewslettersTest < SystemTest
+
+  def test_addresses
+    body = {
+      token: "eovwr",
+      addresses: [
+        {
+          email: "example.864@feedb.in",
+          description: "Example Description"
+        },
+        {
+          email: "jjtid@feedb.in",
+          description: nil
+        }
+      ],
+      tags: ["Favorites", "Newsletters"]
+    }
+
+    CapybaraMock.stub_request(:post, build_url("new_address"))
+      .to_return(body: body.to_json)
+
+    visit "/index.html"
+    sign_in
+    click_tab(:newsletters)
+
+    within("[data-newsletters-target='addressList']") do
+      body[:addresses].each do |address|
+        assert_selector("[data-template='email']", text: address[:email])
+        if address[:description]
+          assert_selector("[data-template='description']", text: address[:description])
+        end
+      end
+    end
+
+    body[:addresses].each_with_index do |address, index|
+      copy_button = find("[data-copy-data-value='#{address[:email]}']")
+
+      if index == 0
+        within(copy_button) do
+          assert_selector("[data-copy-target='copyMessage']", text: "Copy")
+        end
+      end
+
+      copy_button.click
+
+      within(copy_button) do
+        assert_selector("[data-copy-target='copyMessage']", text: "Copied")
+      end
+
+      assert_equal address[:email], copy_button["data-copy-data-value"]
+    end
+
+  end
+end
